b7da7739c135d426d26303523c30d42fe4aa38da
Daisuke Hashimoto <galapagosit@gmail.com>
2016-05-23 21:22:18 +0900

    log test

--- test.js
+++ test.js
@@ -15,11 +15,29 @@
     console.log('hihihi');
   };
 
+  // Functions for block with type 'w' will get a callback function as the
+  // final argument. This should be called to indicate that the block can
+  // stop waiting.
+  ext.wait_random = function (callback) {
+    wait = Math.random();
+    console.log('Waiting for ' + wait + ' seconds');
+    window.setTimeout(function () {
+      callback();
+    }, wait * 1000);
+  };
+
+  ext.power = function (base, exponent) {
+    return Math.pow(base, exponent);
+  };
+
+
   // Block and block menu descriptions
   var descriptor = {
     blocks: [
       // Block type, block name, function name
       [' ', 'log something', 'logging_block'],
+      ['w', 'wait for random time', 'wait_random'],
+      ['r', '%n ^ %n', 'power', 2, 3],
     ]
   };
 
