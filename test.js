011f04481e56803a88c609532c2f82c6de187fc7
Daisuke Hashimoto <galapagosit@gmail.com>
2016-05-23 21:17:25 +0900

    log test

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
-(function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+(function (ext) {
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function () {
+  };
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function () {
+    return {status: 2, msg: 'Ready'};
+  };
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-        ]
-    };
 
-    // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+  ext.logging_block = function () {
+    // Code that gets executed when the block is run
+    console.log('hihihi');
+  };
+
+  // Block and block menu descriptions
+  var descriptor = {
+    blocks: [
+      // Block type, block name, function name
+      [' ', 'log something', 'logging_block'],
+    ]
+  };
+
+  // Register the extension
+  ScratchExtensions.register('Sample extension', descriptor, ext);
 })({});
